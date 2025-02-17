12503517dfe1a9c98443747f1b65d1afb9071910
Champion2005 <patel@apatel.xyz>
2025-02-17 13:49:44 -0500

    project list improvements

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,131 @@
 import React from "react";
+import ChopChop from "../assets/ChopChop.png";
+import EcoWatch from "../assets/EcoWatch.png";
+import Orrery from "../assets/Orrery.png";
+import Remora from "../assets/Remora.png";
+
+const ProjectCard = ({
+  title,
+  desc,
+  shortDesc,
+  techStack,
+  githubLink,
+  webappLink,
+  image,
+  imageColor,
+}) => {
+  const [showDesc, setShowDesc] = React.useState(false);
+
+  const toggleDesc = () => {
+    setShowDesc(!showDesc);
+  };
+
+  return (
+    <div className="bg-background2/30 rounded-2xl">
+      {image && (
+        <>
+          <div className={"flex justify-center max-h-[33%] w-full mb-4 rounded-t-2xl "+imageColor}>
+            <img src={image} alt={title} className="object-scale-down" />
+          </div>
+        </>
+      )}
+      <div className="px-8 py-2">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p className="text-primaryText/75">{techStack}</p>
+        {showDesc ? (
+          <>
+            <p className="text-primaryText/40 mt-8">{desc}</p>
+            <p
+              className="test-primaryText underline hover:cursor-pointer"
+              onClick={toggleDesc}
+            >
+              Show Less
+            </p>
+          </>
+        ) : (
+          <>
+            <p className="text-primaryText/40 mt-8">{shortDesc}</p>
+            <p
+              className="test-primaryText underline hover:cursor-pointer"
+              onClick={toggleDesc}
+            >
+              Read More
+            </p>
+          </>
+        )}
+
+        <div className="flex flex-col gap-0">
+          <a
+            href={githubLink}
+            target="none"
+            className="mt-4 text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline"
+          >
+            -{">"} Github
+          </a>
+          {webappLink && (
+            <a
+              href={webappLink}
+              target="none"
+              className="text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline"
+            >
+              -{">"} Webapp
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const Projects = () => {
-    return (
-        <section
-            id="projects"
-            className="mt-16 px-6 sm:flex-col sm:justify-center w-full text-primaryText"
-        >
-            <h2 className="text-lg sm:text-2xl font-bold mb-4">Projects</h2>
+  return (
+    <section
+      id="projects"
+      className="mt-16 px-6 sm:flex-col sm:justify-center w-full text-primaryText"
+    >
+      <h2 className="text-lg sm:text-2xl font-bold mb-4">Projects</h2>
 
-            <section className="grid grid-cols-2 gap-4">
-                <div className="bg-background2/30 p-8 rounded-2xl hover:scale-[102%] hover:bg-background2/40">
-                    <h3 className="text-lg font-bold">Orrery, Or Are We?</h3>
-                    <p className="text-primaryText/75">Typescript, Three.js, Firebase</p>
-                    <p className="text-primaryText/40 mt-8">Made for NASA Space Apps, this project is an interactive 
-                    webapp with a to scale model of the solar system and accurate orbit simulation.</p>
-
-                    <div className="flex flex-col gap-0">
-                        <a href="https://github.com/jwlebert/spaceapps-2024" target="none" className="mt-3 text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline">-{'>'} Github</a>
-                        <a href="https://spaceapps-2024.web.app/" target="none" className="text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline">-{'>'} Webapp</a>
-                    </div>
-                </div>
-                <div className="bg-background2/30 p-8 rounded-2xl hover:scale-[102%] hover:bg-background2/40">
-                    <h3 className="text-lg font-bold">EcoWatch</h3>
-                    <p className="text-primaryText/75">React, TailwindCSS, Firebase</p>
-                    <p className="text-primaryText/40 mt-8">Made for Winhacks 2024, this is a social networking app with 
-                    a focus on inspiring community oriented enviormental activism by creating community challenges and
-                    leaderboards.</p>
-
-                    <div className="flex flex-col gap-0">
-                        <a href="https://github.com/jwlebert/EcoWatch" target="none" className="mt-3 text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline">-{'>'} Github</a>
-                        <a href="https://ecowatch-c3a72.web.app/" target="none" className="text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline">-{'>'} Webapp</a>
-                    </div>
-                </div>
-                <div className="bg-background2/30 p-8 rounded-2xl hover:scale-[102%] hover:bg-background2/40">
-                    <h3 className="text-lg font-bold">Remora</h3>
-                    <p className="text-primaryText/75">Python</p>
-                    <p className="text-primaryText/40 mt-8">Interpreted programming language with basic features such as 
-                    strings, arrays and functions.</p>
-
-                    <div className="flex flex-col gap-0">
-                        <a href="https://github.com/Champion2005/Remora" target="none" className="mt-3 text-m sm:text-l text-secondaryText/60 hover:text-secondaryText underline">-{'>'} Github</a>
-                    </div>
-                </div>
-            </section>
-        </section>
-    );
+      <section className="grid grid-cols-2 gap-4">
+        <ProjectCard
+          title="Chop Chop"
+          desc="Engaging fullstack web app designed for studying. With a features like a flashcard tool (you can create flashcard sets and use them to study), as well as a todo list and challenging quizzes to help you prepare. Gain points by using the site and unlock achievements."
+          shortDesc="Engaging fullstack web app..."
+          techStack="React, Vite, Firebase, HTML/CSS, Git"
+          githubLink="https://github.com/Kataray/Winhacks2025"
+          webappLink="https://winhacks-2025.web.app/"
+          image={ChopChop}
+            imageColor="bg-amber-100"
+        />
+        <ProjectCard
+          title="Orrery, or Are We?"
+          desc="Made for NASA Space Apps, this project is an interactive webapp with a to scale model of the solar system and accurate orbit simulation."
+          shortDesc="Made for NASA Space Apps..."
+          techStack="ThreeJS, TypeScript, Vite, Firebase, HTML/CSS, Git"
+          githubLink="https://github.com/jwlebert/spaceapps-2024"
+          webappLink="https://spaceapps-2024.web.app/"
+          image={Orrery}
+          imageColor="bg-amber-300"
+        />
+        <ProjectCard
+          title="EcoWatch"
+          desc="Made for Winhacks 2024, this is a social networking app with a focus on inspiring community oriented enviormental activism by creating community challenges and leaderboards."
+          shortDesc="Made for Winhacks 2024..."
+          techStack="React, Vite, Firebase, Tailwind CSS, Git"
+          githubLink="https://github.com/jwlebert/EcoWatch"
+          webappLink="https://ecowatch-c3a72.web.app/"
+          image={EcoWatch}
+          imageColor="bg-amber-700"
+        />
+        <ProjectCard
+          title="Remora"
+          desc="Interpreted programming language with basic features such as strings, arrays and functions."
+          shortDesc="Interpreted programming language..."
+          techStack="Python"
+          githubLink="https://github.com/Champion2005/Remora"
+          image={Remora}
+          imageColor="bg-amber-900"
+        />
+      </section>
+    </section>
+  );
 };
